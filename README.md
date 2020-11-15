f085e12b6547dff13ce663618e7f6720012a68e7
‘smilecamo99’ <yangyongju@shketai.com>
2020-11-15 21:35:15 +0800

    学习vue3

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # vue_demo
-
-#### vue_shop
-
-基于 vue 全家桶制作购物网站
---
+++ vue3/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <column-list :list="list" class=""></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "this is title1",
+    description: "this is description,this is description",
+    // avator:
+    //   "https://pic2.zhimg.com/v2-640d245f4cffa249560b36e7b478a0f3_xs.jpg?source=1940ef5c",
+  },
+  {
+    id: 2,
+    title: "this is title3",
+    description: "this is 3,this is 3",
+    avator:
+      "https://pic2.zhimg.com/v2-640d245f4cffa249560b36e7b478a0f3_xs.jpg?source=1940ef5c",
+  },
+  {
+    id: 3,
+    title: "this is title55555",
+    description: "this is 3,this is 3",
+    avator:
+      "https://pic2.zhimg.com/v2-640d245f4cffa249560b36e7b478a0f3_xs.jpg?source=1940ef5c",
+  },
+];
+export default defineComponent({
+  name: "App",
+  components: {
+    ColumnList,
+  },
+  setup() {
+    return {
+      list: testData,
+    };
+  },
+});
+</script>
+
+<style></style>
