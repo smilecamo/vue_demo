caed1109d9590800afc2e1f8cf4c6cbee9763bdb
杨永举 <>
2020-12-11 13:40:57 +0800

    新建 vue3分支

---
+++ vue3_demo/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>
+    {{ count }}
+  </h3>
+  <h3>
+    {{ info.name }}
+  </h3>
+  <button @click="countAction">jiajia</button>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+export default {
+  setup() {
+    const count = ref(0);
+    const info = reactive({
+      name: "lilei",
+      age: 17,
+    });
+    const countAction = () => {
+      count.value = count.value += 1;
+    };
+    return {
+      count,
+      info,
+      countAction,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
