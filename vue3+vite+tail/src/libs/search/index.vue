e18ab34fccad33bbd845d60de340e8a448c2a6fb
yang <1243354771@qq.com>
2022-08-31 18:40:09 +0800

    header

---
+++ vue3+vite+tail/src/libs/search/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="group relative p-0.5 border rounded-xl border-white duration-500 hover:bg-red-100/40">
+    <div>
+    <m-svg-icon 
+    name="search"
+    color="#707070"
+    class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] left-2"></m-svg-icon>
+    <input 
+    class=" block w-full text-sm text-zinc-900 pl-4 h-[44px] rounded-lg outline-none bg-zinc-100 group-hover:bg-white"
+    type="text" placeholder="搜索" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+// css group 分组css group-input 分组下的css
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue3+vite+tail/src/views/layout/components/header/index.vue
+++ vue3+vite+tail/src/views/layout/components/header/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-header
+  <div class="w-screen bg-white border-b border-b-zinc-100">
+    <div class="flex items-center">
+      <img class="h-[40px] mr-1" src="https://file.ipadown.com/tophub/assets/images/logo.png">
+      <SearchVue class="mr-1 w-full" ></SearchVue>
+      <MyVue class="mr-1"></MyVue>
+      <ThemeVue></ThemeVue>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import SearchVue from './search/index.vue'
+import MyVue from './my.vue'
+import ThemeVue from './theme.vue'
 </script>
 
 <style lang="scss" scoped>
